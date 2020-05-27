d07b0484fb0b6482d566973d394f1a39697d3aaa
Jacob Stimes <jacob.stimes@gmail.com>
2020-05-27 01:45:10 -0700

    Add documentation to volume

--- src/app/processing/volume.ts
+++ src/app/processing/volume.ts
@@ -1,8 +1,13 @@
 import { vec3, vec4, mat4 } from 'src/app/gl-matrix.js';
 import { Square, Triangle, makeVec, addVec, Point, getTrianglesFromSquares } from 'src/app/math_utils';
 
+// TODO - don't just store a boolean! Fist just store average or interpolation 
+// or something more specific, and decide what to do with it when converting to 
+// a mesh.
+
+/** Collection of all sampled images in a stack. */
 export class Volume {
-    z: Slice[] = [];
+    readonly z: Slice[] = [];
 
     isPt(pt: vec3): boolean {
         return this.z[pt[2]].y[pt[1]].x[pt[0]];
@@ -15,10 +20,16 @@ export class Volume {
     }
 }
 
+/** Collection of all sampling rows of an entire image. */
 export interface Slice {
-    y: Row[];
+    readonly y: Row[];
 }
 
+/** Represents a row of samplings of a single image. */
 export interface Row {
-    x: boolean[];
+    /** 
+     * Each element represents whether that sampled section of the row  is 
+     * above the rendering threshold.
+     */
+    readonly x: boolean[];
 }
